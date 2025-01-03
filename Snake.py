8049ae38f0c626c4f2d956e881620e8af5fa86ee
Abhijit Kalita <158345729+Abhi-987@users.noreply.github.com>
2025-01-03 18:30:52 +0530

    Add files via upload

---
+++ Snake.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+from snake_game import Snake
+import time
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(.20)
+    snake.move()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend_seg()
+        score.score_count()
+
+    # collision with the wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        score.reset()
+        snake.reset()
+
+    # collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            score.reset()
+            snake.reset()
+
+screen.exitonclick()
